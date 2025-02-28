c108797ac64d284d6e9587c136f50346f16f2855
Sufyan Mun <60675257+sufyanmun@users.noreply.github.com>
2025-02-27 22:12:37 -0800

    Added transaction text and amount hooks

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useState } from "react";
 
 const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
   return (
     <>
       <h3>Add new transaction</h3>
@@ -10,6 +13,8 @@ const AddTransaction = () => {
           <input
             type="text"
             id="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
             placeholder="Enter Transaction Type..."
           />
         </div>
@@ -18,9 +23,15 @@ const AddTransaction = () => {
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
         </div>
-        <button class="btn">Add transaction</button>
+        <button className="btn">Add transaction</button>
       </form>
     </>
   );
