182d74f40f1b76fa2f630a0410db16fef4e8ea15
Sufyan Mun <60675257+sufyanmun@users.noreply.github.com>
2025-03-01 17:16:39 -0800

    Added delete functionality

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,12 @@
 export default (state, Action) => {
   switch (Action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== Action.payload
+        ),
+      };
     default:
       return state;
   }
