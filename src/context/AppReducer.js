f3ca6cb6f45e3fcd6b4c8211d531f0260d554f3e
Sufyan Mun <60675257+sufyanmun@users.noreply.github.com>
2025-03-02 01:59:07 -0800

    Added add transcation component and functionality

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,11 +1,16 @@
-export default (state, Action) => {
-  switch (Action.type) {
+export default (state, action) => {
+  switch (action.type) {
     case "DELETE_TRANSACTION":
       return {
         ...state,
         transactions: state.transactions.filter(
-          (transaction) => transaction.id !== Action.payload
+          (transaction) => transaction.id !== action.payload
         ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
       };
     default:
       return state;
